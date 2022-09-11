89e213b86f1bb475741706dd3e5af3efda1f7638
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-11 00:16:57 -0300

    [feat] App import zone componentized

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
+import ImportZone from './components/Import';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        1. import spreadsheet
-        {' '}
-        {' '}
-        <button
-          type="button"
-          onClick={
-            () => console.log('click')
-          }
-        >
-          Import CSV
-
-        </button>
-      </div>
+      {ImportZone()}
       <div>select group column</div>
       <div>define quantities for card</div>
       <div>save configs</div>

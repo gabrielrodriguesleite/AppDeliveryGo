587ef35bd665dace374d8b51d8db6f1108be2501
Gabriel Rodrigues Leite <gabrielrodrigues@klever.io>
2022-09-10 23:24:55 -0300

    [feat] App scope

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>import spreadsheet</div>
+      <div>
+        1. import spreadsheet
+        {' '}
+        {' '}
+        <button
+          type="button"
+          onClick={
+            () => console.log('click')
+          }
+        >
+          Import CSV
+
+        </button>
+      </div>
       <div>select group column</div>
       <div>define quantities for card</div>
       <div>save configs</div>
-      <div>deck
+      <div>
+        deck
         <div>cards</div>
       </div>
     </div>

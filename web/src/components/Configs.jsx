edd3c063bf1ae2c2ec1eff5a17b9b40237083f70
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-11 00:16:58 -0300

    [feat] define quantities for card

--- web/src/components/Configs.jsx
+++ web/src/components/Configs.jsx
@@ -10,7 +10,12 @@ export default function ConfigsZone() {
         <select><option>Opção 1</option></select>
       </div>
 
-      <div>3. define quantities for card</div>
+      <div>
+        3. define quantities for card
+        {' '}
+        <input type="number" min="1" max="100" defaultValue="5" />
+      </div>
+
       <div>4. save configs</div>
 
     </div>

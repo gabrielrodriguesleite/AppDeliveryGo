cc442685b1922aeb0bfe2b5782272dc381852b14
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-11 00:16:58 -0300

    [feat] ConfigZone added

---
+++ web/src/components/Configs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ConfigsZone() {
+  return (
+    <div>
+
+      <div>2. select group column</div>
+      <div>3. define quantities for card</div>
+      <div>4. save configs</div>
+
+    </div>
+  );
+}

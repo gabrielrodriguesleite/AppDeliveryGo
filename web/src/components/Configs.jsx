25b34023c7855b5f3031233131a1fd19812a3cc3
Gabriel Rodrigues Leite <gabrielrodrigues@klever.io>
2022-09-10 23:37:55 -0300

    [feat] select group

--- web/src/components/Configs.jsx
+++ web/src/components/Configs.jsx
@@ -4,7 +4,12 @@ export default function ConfigsZone() {
   return (
     <div>
 
-      <div>2. select group column</div>
+      <div>
+        2. select group column
+        {' '}
+        <select><option>Opção 1</option></select>
+      </div>
+
       <div>3. define quantities for card</div>
       <div>4. save configs</div>
 

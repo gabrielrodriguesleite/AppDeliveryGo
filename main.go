5a406c04cf38d0970b2c229b20dc930f6e9f0d4a
Gabriel Rodrigues Leite <gabrielrodrigues@klever.io>
2022-09-10 21:38:01 -0300

    [feat] TODO atualizado

--- main.go
+++ main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Printf("AppDeliveryGo v%d.%d.%d\n", 0, 0, 1)
 }
+
+// TODO:
+/*
+
++ Serve uma página web
++ Carrega dados csv
++ Acessa API google maps pra gerar(s) mapa(s)
++ Imprimir(PDF)
+
+*/
